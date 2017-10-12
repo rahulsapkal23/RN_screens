98bab54f6dc745ed6889a13fcc3d9b5488df2fc8
rahul.sapkal <rahul.sapkal@neosofttech.com>
2017-10-12 17:34:14 +0530

    Added second screen with All UI

--- app/screen/screen2.js
+++ app/screen/screen2.js
@@ -10,6 +10,8 @@ import {
 } from 'react-native';
 
 import styles from './../styles/screen2';
+import Dash from 'react-native-dash';
+import ScrollableTabView, { ScrollableTabBar, } from 'react-native-scrollable-tab-view';
 
 export default class screen2 extends Component {
 
@@ -25,34 +27,168 @@ export default class screen2 extends Component {
 
                     <ScrollView contentContainerStyle={styles.SWcontainer}>
                        <View style={styles.VWcontainer1}>
-     
+      <View style={styles.VWcontainer11}>
+         <Text style={styles.TextStyle1}>182</Text>
+         <Text style={styles.TextStyle2}>Available quota</Text>
 </View>
+ <View style={styles.VWcontainer12}>
+         <Text style={styles.TextStyle3}>End</Text>
+         <Text style={styles.TextStyle4}>2016.12.12</Text>
+
+</View>
+</View>
+<Dash style={{width:"100%", height:1,marginTop:1}} dashColor='rgba(61, 179, 250, 100)'/>
+    {/* <View style={styles.dash}>
+</View> */}
+
 <View style={styles.VWcontainer2}>
-     
 </View>
-<View style={styles.VWcontainer3}>
-     
+<View style={styles.VWcontainer31}>
+  <ScrollableTabView
+    style={{marginTop:0,   }}
+    initialPage={0}
+    renderTabBar={() => <ScrollableTabBar />}
+  >
+       
+  
+      <ScrollView tabLabel='Sun'style={styles.tabView}>
+         <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
 </View>
+<View style={styles.VWcontainer7}>
+
 <View style={styles.VWcontainer4}>
    <View style={styles.VWcontainer5}>
      <View style={styles.VWcontainer6}>
-     
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
 </View> 
 </View>  
 </View>
+</View></View>
+
+      </ScrollView>
+         <ScrollView tabLabel='Mon'style={styles.tabView}>
+            <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
+</View>
+<View style={styles.VWcontainer7}>
+
+<View style={styles.VWcontainer4}>
+   <View style={styles.VWcontainer5}>
+     <View style={styles.VWcontainer6}>
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
+</View> 
+</View>  
+</View>
+</View></View>
+         </ScrollView>
+          <ScrollView tabLabel='Tue' style={styles.tabView}>
+            <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
+</View>
+<View style={styles.VWcontainer7}>
+
+<View style={styles.VWcontainer4}>
+   <View style={styles.VWcontainer5}>
+     <View style={styles.VWcontainer6}>
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
+</View> 
+</View>  
+</View>
+</View></View>
+          </ScrollView>
+           <ScrollView tabLabel='Wed' style={styles.tabView}>
+              <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
+</View>
+<View style={styles.VWcontainer7}>
+
+<View style={styles.VWcontainer4}>
+   <View style={styles.VWcontainer5}>
+     <View style={styles.VWcontainer6}>
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
+</View> 
+</View>  
+</View>
+</View></View>
+           </ScrollView>
+             <ScrollView tabLabel='Thu' style={styles.tabView}>
+                <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
+</View>
+<View style={styles.VWcontainer7}>
+
+<View style={styles.VWcontainer4}>
+   <View style={styles.VWcontainer5}>
+     <View style={styles.VWcontainer6}>
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
+</View> 
+</View>  
+</View>
+</View></View>
+             </ScrollView>
+             <ScrollView tabLabel='Fri' style={styles.tabView}>
+                <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
+</View>
+<View style={styles.VWcontainer7}>
+
+<View style={styles.VWcontainer4}>
+   <View style={styles.VWcontainer5}>
+     <View style={styles.VWcontainer6}>
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
+</View> 
+</View>  
+</View>
+</View></View>
+             </ScrollView>
+              <ScrollView tabLabel='Sat' style={styles.tabView}>
+                 <View style={styles.VWcontainer311}>
+        <View style={styles.VWcontainer3}>
+     <Dash style={{width:1,  flexDirection:'column', height:60}} dashColor='rgba(61, 179, 250, 100)'/>
+</View>
+<View style={styles.VWcontainer7}>
+
+<View style={styles.VWcontainer4}>
+   <View style={styles.VWcontainer5}>
+     <View style={styles.VWcontainer6}>
+      <Text style={styles.TextStyle6}>549</Text>
+         <Text style={styles.TextStyle7}>Day collection</Text>
+</View> 
+</View>  
+</View>
+</View></View>
+                </ScrollView> 
+               </ScrollableTabView>
+</View>
 
 <View style={styles.SWcontainer123}>
+<View style={styles.VWcontainer422}>
+  
+      <Text style={styles.TextStyle8}> 12.12-12.19 (Term of validity)</Text>
+      <Text style={styles.TextStyle9}> Discounts are available </Text>
+      <Text style={styles.TextStyle9}> this week </Text>
+      
+  </View>
+ 
       <TouchableOpacity  onPress={() => this.props.navigation.navigate('screen1')}
         style={styles.BtnContainer}>
                                 <Text style={styles.BtnTxt}>Package</Text>
                             </TouchableOpacity>
 </View>
-<View style={styles.SWcontainer123}>
-      <TouchableOpacity  onPress={() => this.props.navigation.navigate('screen1')}
-        style={styles.BtnContainer}>
-                                <Text style={styles.BtnTxt}>Package</Text>
-                            </TouchableOpacity>
-</View>
+
                                </ScrollView>
          </View>      
     );
--- app/styles/screen2.js
+++ app/styles/screen2.js
@@ -6,47 +6,168 @@ export default StyleSheet.create({
  VWcontainer: {
        
         borderColor: 'blue', borderWidth: 1,
-        backgroundColor: 'transparent',
-         flexDirection: 'row',
-        flex:1
+        backgroundColor: 'white',
+        //  flexDirection: 'row',
+         flex:1
+        
+    },
+         tabView: {
+       
+             
+        // borderColor: 'pink', borderWidth: 1,
+
+      
+        backgroundColor: 'white',
+        height:100,
+        width:"100%"
+        
     },
 
          VWcontainer1: {
        
-        borderColor: 'red', borderWidth: 1,
+              flexDirection: 'row',
+
+        // borderColor: 'red', borderWidth: 1,
         backgroundColor: 'transparent',
-        height:60,
+        height:70,
         width:"100%"
+        
+    },
+      TextStyle1: {
+        color: 'rgba(61, 179, 250, 100)', 
+        fontWeight: 'bold',
+        fontSize: 33,
+       
+    },
+       TextStyle6: {
+        color: 'rgba(81, 81, 81, 100)', 
+        fontWeight: 'bold',
+        fontSize: 41,
+       
+    },
+     TextStyle7: {
+        color: 'rgba(164, 179, 202, 100)', 
+       
+        fontSize: 12,
+       marginTop:0
+    },
+     TextStyle2: {
+        color: 'rgba(153, 153, 153, 100)', 
+       
+        fontSize: 13,
+       marginTop:0
+    },
+      TextStyle3: {
+         color: 'rgba(153, 153, 153, 100)', 
+       left:75,
+        fontSize: 13,
+       
+    },
+  
+     TextStyle4: {
+        color: 'rgba(255, 138, 0, 100)', 
+       
+        fontSize: 20,
+       marginTop:0
+    },
+       TextStyle5: {
+         color: 'rgba(153, 153, 153, 100)', 
+       marginLeft:22,
+        fontSize: 15,
+       
+    },
+      TextStyle8: {
+         color: 'rgba(255, 138, 0, 100)', 
+       marginBottom:5,
+       marginLeft:22,
+        fontSize: 14,
+       
+    },
+     TextStyle9: {
+         color: 'rgba(81, 81, 81, 100)', 
+         
+       marginLeft:22,
+        fontSize: 14,
+       
+    },
+       VWcontainer11: {
+        alignItems: 'center',
+        // borderColor: 'blue', borderWidth: 1,
+        backgroundColor: 'transparent',
+        top:10,
+        left:20,
+        height:59,
+        width:100
+        
+    },
+         VWcontainer422: {
+       
+        // borderColor: 'blue', borderWidth: 1,
+        backgroundColor: 'transparent',
+       
+        top:20,
+        height:60,
+        width:220
+        
+    },
+      VWcontainer12: {
+       
+        // borderColor: 'red', borderWidth: 1,
+        backgroundColor: 'transparent',
+        top:15,
+        left:154,
+        height:44,
+        width:100
         
     },
       VWcontainer2: {
        
-        borderColor: 'pink', borderWidth: 1,
-        backgroundColor: 'pink',
+        // borderColor: 'pink', borderWidth: 1,
+        backgroundColor: 'white',
         height:85,
         width:"100%"
         
     },
-        VWcontainer3: {
-       
-        borderColor: 'pink', borderWidth: 1,
-        backgroundColor: 'blue',
-        height:21,
+        VWcontainer31: {
+       flexDirection:"row",
+        // borderColor: 'blue', borderWidth: 1,
+        backgroundColor: 'white',
+        height:250,
+        width:"100%"
+        
+    },
+       VWcontainer311: {
+    //    flexDirection:"row",
+//  alignItems: 'center',
+        // borderColor: 'pink', borderWidth: 1,
+        backgroundColor: 'white',
+        height:300,
         width:"100%"
         
     },
          VWcontainer3: {
-       
-        borderColor: 'pink', borderWidth: 1,
-        backgroundColor: 'yellow',
+        alignItems: 'center',
+        left:120,
+        // borderColor: 'pink', borderWidth: 1,
+        backgroundColor: 'white',
         height:60,
         width:"100%"
         
     },
+     VWcontainer7: {
+        alignItems: 'center',
+        left:120,
+        
+        // borderColor: 'pink', borderWidth: 1,
+        backgroundColor: 'white',
+        height:150,
+        width:"100%"
+        
+    },
          VWcontainer4: {
-       
-        borderColor: 'pink', borderWidth: 1,
-        backgroundColor: 'red',
+        alignItems: 'center',
+        borderColor: 'rgba(243, 245, 249, 100)',  borderWidth: 1,
+        backgroundColor: 'white',
         height:140,
         width:140,
         borderRadius:70,
@@ -54,9 +175,9 @@ export default StyleSheet.create({
         
     },
       VWcontainer5: {
-       
-        borderColor: 'yellow', borderWidth: 1,
-        backgroundColor: 'blue',
+        alignItems: 'center',
+        borderColor: 'rgba(242, 244, 249, 100)',  borderWidth: 1,
+        backgroundColor: 'white',
         height:115,
         width:115,
         borderRadius:57.5,
@@ -64,26 +185,29 @@ export default StyleSheet.create({
         
     },
       VWcontainer6: {
+        alignItems: 'center',
        
-        borderColor: 'yellow', borderWidth: 1,
+        // borderColor: 'yellow', borderWidth: 1,
         backgroundColor: 'white',
-        height:58,
-        width:77,
+        height:73,
+        width:85,
         margin:20
         
     },
      SWcontainer: {
-        alignItems: 'center',
-        borderColor: 'black', borderWidth: 1,
-        
-       height:"100%",
-       width:"100%"
+          alignItems: 'center',
+    //    scrollEnabled:true,
+        // borderColor: 'black', borderWidth: 1,
+        // flex:1
+
+     
     },
        SWcontainer123: {
-           top:140,
+           flexDirection:'row',
+           top:100,
        height:100,
         width:"100%",
-        borderColor: 'black', borderWidth: 1,
+        // borderColor: 'black', borderWidth: 1,
        
     },
  BtnTxt: {
@@ -91,14 +215,23 @@ export default StyleSheet.create({
        
         fontSize: 18
     },
+    dash:{width:"100%", 
+     borderColor: 'rgba(61, 179, 250, 100)', borderWidth: 2,
+        backgroundColor: 'white',
+       
+
+        borderStyle:'dotted',
+        width:"100%" ,height:0,  
+    },
+
         BtnContainer: {
         backgroundColor: 'rgba(61, 179, 250, 100)', 
         borderColor: '#3DB3FA',
         borderWidth: 10,
         borderRadius: 20,
         alignItems: 'center',
-    //    top:309,
-    //    left:733,
+       top:30,
+       marginLeft:5,
         height: 42,
         width: 120
     },
